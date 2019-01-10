ddd1834166ef7ae850ccaa26541c476b4a70ac93
arebuelta <42614989+arebuelta@users.noreply.github.com>
2019-01-10 01:39:36 -0800

    sort

--- Labs/Lab 109/sketch.js
+++ Labs/Lab 109/sketch.js
@@ -6,7 +6,6 @@
 
 // global variables
 var array = [];
-var Smallsub;
 
 // setup function
 function setup(){
@@ -14,21 +13,24 @@ function setup(){
   for (var i = 0; i < 10; i++){
   array[i] = random(10);
   }
-  sort(array);
+  sort(array); // calls sort function
   console.log(array);
 }
 
 // sort function
 function sort(array){
+  // for loop that sorts values in array
   for (var i = 0; i < array.length-1; i++){
-    var Smallsub = i;
+    var SmallSub = i; // variable holding i value
+	// for loop that compares the values in the array
     for (var j = i+1; j < array.length-1; i++){
-    if (array[j] < array[Smallsub]){
-    Smallsub = j
+    // if the value in the array is smaller than the previous value, SmallSub is assigned j
+	if (array[j] < array[SmallSub]){
+    SmallSub = j
       }
     }
-    var temp = array[i];
-    array [i] = array[Smallsub];
-    array[Smallsub] = temp;
+    var temp = array[i]; // temp variable holding array[i]
+    array[i] = array[SmallSub]; // array[i] is assigned array[SmallSub]
+    array[SmallSub] = temp; // array[SmallSub] is assigned temp
   }
 }
