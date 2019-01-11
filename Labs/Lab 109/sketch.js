1955c467e73e72a72879acfdf87cdc5c1d2a29e3
arebuelta <42614989+arebuelta@users.noreply.github.com>
2019-01-11 01:38:04 -0800

    sort

--- Labs/Lab 109/sketch.js
+++ Labs/Lab 109/sketch.js
@@ -21,16 +21,17 @@ function setup(){
 function sort(array){
   // for loop that sorts values in array
   for (var i = 0; i < array.length-1; i++){
-    var SmallSub = i; // variable holding i value
-	// for loop that compares the values in the array
-    for (var j = i+1; j < array.length-1; i++){
-    // if the value in the array is smaller than the previous value, SmallSub is assigned j
-	if (array[j] < array[SmallSub]){
-    SmallSub = j
-      }
-    }
-    var temp = array[i]; // temp variable holding array[i]
-    array[i] = array[SmallSub]; // array[i] is assigned array[SmallSub]
-    array[SmallSub] = temp; // array[SmallSub] is assigned temp
+	// swaps array value if the first value is greater than the next value
+    if (array[i] > array[i+1]){
+		var temp = array[i]; // temp variable holding array[i]
+		array[i] = array[i+1];
+		array[i+1] = temp
+	}
+  }
+  // checks if the array is sorted and if it is not it starts the sort function again
+  for (var i = 0; i < array-length-1; i++){
+	  if (array[i] > array[i+1]){
+		  sort(array); // calls sort function
+	  }
   }
 }
