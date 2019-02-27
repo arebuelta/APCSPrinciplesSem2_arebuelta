8e46300f81218b1560bbbbd6f57fb5f9c05ae0d7
arebuelta <42614989+arebuelta@users.noreply.github.com>
2019-02-27 08:50:47 -0800

    art

--- Labs/Lab824 BallTwo/ball.js
+++ Labs/Lab824 BallTwo/ball.js
@@ -6,12 +6,10 @@
 
 function Ball(loc, rad, col, id){
   // Instance variables
-  this.locX = locX;
-  this.locY = locY;
-  this.speedX = random(-3.0, 3.0);
-  this.speedY = random(-3.0, 3,0);
+  this.loc = loc;
+  this.vel = createVector(random(-1.0, 1.0),  random(-1.0, 1.0));
   this.rad = rad;
-  this.acc = createVector(0, .1);
+  this.acc = createVector(0, 0.01);
   this.id = id;
   // this.col = col;
   // This function calls other functions
@@ -28,10 +26,10 @@ function Ball(loc, rad, col, id){
   }
   //checkEdges() reverses speed when the ball touches an edge
   this.checkEdges = function(){
-    if(this.locX < 0) this.speedX = -this.speedX;
-    if(this.locX > width) this.speedX = -this.speedX;
-    if(this.locY < 0) this.speedY = -this.speedY;
-    if(this.locY > height) this.speedY = -this.speedY;
+    if(this.loc.x < 0) this.vel.x = -this.vel.x;
+    if(this.loc.x > width) this.vel.x = -this.vel.x;
+    if(this.loc.y < 0) this.vel.y = -this.vel.y;
+    if(this.loc.y > height) this.vel.y = -this.vel.y;
   }
   // render() draws the ball at the new location
   this.render = function(){
@@ -41,6 +39,15 @@ function Ball(loc, rad, col, id){
     else{
       stroke(155, 155, 0);
     }
-    line(this.loc.x, this.loc.y, rad, rad, 125);
+    if (id < Balls.length-1){
+      line(this.loc.x, this.loc.y, Balls[id+1].loc.x, Balls[id+1].loc.y, 125);
+    }
+    else{
+      line(this.loc.x, this.loc.y, Balls[0].loc.x, Balls[0].loc.y, 125);
+    }
+    fill(255);
+    noStroke();
+    ellipse(this.loc.x, this.loc.y, rad, rad);
+
   }
 }
--- Labs/Lab824 BallTwo/sketch.js
+++ Labs/Lab824 BallTwo/sketch.js
@@ -5,18 +5,19 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
-  loadBalls(12);
+  loadBalls(150);
 }
 function draw() {
+  background(20, 20, 20, 20);
   for (i = 0; i < Balls.length; i++){
   Balls[i].run();
   }
 }
 function loadBalls(numBalls){
   for (i = 0; i < numBalls; i++){
-      var loc = random(800)
-      var r = random(20, 40);
+      var loc = createVector(random(800), random(800));
+      var r = 10;
       var col = color(random(255), random(255), random(255));
-  Balls.push(new Ball(x, y, r, col, i))
+  Balls.push(new Ball(loc, r, col, i))
 }
 }
