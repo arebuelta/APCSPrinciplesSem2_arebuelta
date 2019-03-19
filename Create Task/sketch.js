3dddefcd89e4f4c29dddd0a46f7894e5f99bc0bc
arebuelta <42614989+arebuelta@users.noreply.github.com>
2019-03-19 08:49:43 -0700

    APCS Tasks

---
+++ Create Task/sketch.js
@@ -0,0 +1,21 @@
+/*
+** Anthony Rebuelta
+** Create Task: Data Sort
+** 19 March 2019
+*/
+
+var population;
+
+function preload(){
+  population = loadJSON("population.json");
+}
+
+function setup(){
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(0);
+}
+
+function draw(){
+
+}
