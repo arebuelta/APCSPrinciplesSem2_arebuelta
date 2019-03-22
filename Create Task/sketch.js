a2e2d093a9aea03d20b5f5632ae827cd710e472e
arebuelta <42614989+arebuelta@users.noreply.github.com>
2019-03-22 08:48:47 -0700

    create task

--- Create Task/sketch.js
+++ Create Task/sketch.js
@@ -1,21 +1,70 @@
 /*
 ** Anthony Rebuelta
-** Create Task: Data Sort
+** Create Task: Population Data
 ** 19 March 2019
 */
 
-var population;
+var data;
+var totalPop = 0;
 
 function preload(){
-  population = loadJSON("population.json");
+  data = loadJSON("population.json");
 }
 
 function setup(){
-  var cnv = createCanvas(800, 800);
+  var cnv = createCanvas(1000, 1000);
   cnv.position((windowWidth-width)/2, 30);
   background(0);
+  sum();
 }
 
 function draw(){
+  display();
+}
 
+function sort(){
+  // for loop that sorts values in array
+  for (var i = 0; i < array.length-1; i++){
+	// swaps array value if the first value is greater than the next value
+    if (array[i] > array[i+1]){
+		var temp = array[i]; // temp variable holding array[i]
+		array[i] = array[i+1];
+		array[i+1] = temp;
+	}
+  }
+  // checks if the array is sorted and if it is not it starts the sort function again
+  for (var i = 0; i < array-length-1; i++){
+	  if (array[i] > array[i+1]){
+		  sort(array); // calls sort function
+	  }
+  }
+  }
+
+function display(){
+  fill(255);
+  textSize(20);
+  text("State", 35, 30);
+  text("Population", 250, 30);
+  text("% of Population Growth", 450, 30);
+  text("% of total US Population", 725, 30);
+  textSize(15);
+  for (var i = 0; i < data.usPopulationData.length; i++){
+    fill(255);
+    text(data.usPopulationData[i].state, 35, 75+(17*(i-1)));
+    text(data.usPopulationData[i].population, 265, 75+(17*(i-1)));
+    fill(255, 0, 0);
+    if (data.usPopulationData[i].growth < 0){
+
+    }
+    else {
+      fill(0, 255, 0)
+      rect(475, 60+(17*(i-1)), data.usPopulationData[i].growth*(100), 15)
+    }
+  }
+}
+
+function sum(){
+  for (var i = 0; i < data.usPopulationData.length; i++){
+    totalPop = totalPop + data.usPopulationData[i].population;
+  }
 }
